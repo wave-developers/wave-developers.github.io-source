d926e081328be24e82d7fc922adb80fbe315d53e
Eugene Krevenets <hyzhak@ya.ru>
2015-04-02 02:12:29 +0200

    install assembly

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,34 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        config: grunt.file.readYAML('./config.ygml')
+        pkg: grunt.file.readJSON('package.json'),
+        config: grunt.file.readYAML('./config.yml'),
+        theme: 'themes/<%= config.theme %>',
+
+        assemble: {
+            options: {
+                flatten: true,
+                data: 'config.yml',
+                prettify: {
+                    indent: 2,
+                    condense: true,
+                    newlines: true
+                },
+                assets: '_demo/assets',
+                helpers: 'helpers/*.js',
+                partials: ['<%= theme %>/includes/*.hbs'],
+                layoutdir: '<%= theme %>/layouts',
+                layout: 'default.hbs'
+            },
+            'wave-developers': {
+                files: [
+                    { '<%= config.site.build %>/': ['pages/*.hbs'] },
+                    { expand: true, src: ['posts/*.md'], dest: '<%= config.site.build %>/'}
+                ]
+            }
+        }
     });
+
+    grunt.loadNpmTasks('assemble');
+
+    grunt.registerTask('default', ['assemble']);
 };
